6e40b1caeb32115eaa299dadf7343e8b36cfd3b2
Jannis Frank <frank.jannis@gmail.com>
2025-07-04 12:40:37 +0200

    zoxide for dor aliases

--- config.fish
+++ config.fish
@@ -14,13 +14,13 @@ alias ls='eza -al --color=always --group-directories-first --icons' # preferred 
 # alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
 # alias ll='eza -l --color=always --group-directories-first --icons'  # long format
 alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
-alias l.="eza -a | grep -e '^\.'"                                     # show only dotfiles
+# alias l.="eza -a | grep -e '^\.'"                                     # show only dotfiles
 
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-alias .....='cd ../../../..'
-alias ......='cd ../../../../..'
+alias ..='z ..'
+alias ...='z ../..'
+alias ....='z ../../..'
+alias .....='z ../../../..'
+alias ......='z ../../../../..'
 
 # Recent installed packages
 alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
